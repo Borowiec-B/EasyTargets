acc8ba7ce57401064dd23b2005c6fd0e673b1cfb
throwaway47912 <throwaway47912@gmail.com>
2021-01-04 15:08:49 +0100

    Add -r/--run option
    -r/--run searches upwards for closest .target.sh (not customizable yet),
    cd's into its parent directory, and executes it.

--- EasyTargets.sh
+++ EasyTargets.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/sh
 
-options=""
-longopts=""
+options="r"
+longopts="run"
 progname="EasyTargets"
 new_args="$(getopt --quiet --options "$options" --longoptions "$longopts" --name "$progname" -- "$@")"
 getopt_status=$?
-
-usage="Usage: (to be written)"
 
 if [ $getopt_status -ne 0 ]; then
 	echo -e "Invalid options.\n"
@@ -14,10 +12,32 @@ if [ $getopt_status -ne 0 ]; then
 	exit 1
 fi
 
+usage="Usage: (to be written)"
+
+run_selected_cmd() {
+	default_filename=".target.sh"
+	cmd_filename="$1"
+
+	if [ -z "$cmd_filename" ]; then
+		cmd_filename="$default_filename"
+	fi
+
+	# Upfind returns an absolute path
+	cmd_filepath="$(./utilities/call_wrapper.py upfind "$cmd_filename")"
+
+	cd "$(dirname "$cmd_filepath")"
+	"$cmd_filepath"
+}
+
+
 eval set -- "$new_args"
 
 while true; do
 	case "$1" in
+		"-r"|"--run")
+			run_selected_cmd
+			break
+			;;
 		--)
 			shift
 			break
